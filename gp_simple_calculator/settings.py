e6005bcbcc77105ae68c0d39939482b0b5e86108
Prudentdev-xyz <your-github-email@gmail.com>
2026-05-05 22:08:14 +0100

    Host redeploy

--- gp_simple_calculator/settings.py
+++ gp_simple_calculator/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%^i7hd-(_0qpcofx3%bl+t72o3$l))4(+-q7vt#iffd$di$q%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gpsimplecalculator-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
